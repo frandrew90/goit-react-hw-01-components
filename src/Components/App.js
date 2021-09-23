ca4316a51a96d54082246a1277d2f73779d7ae47
frandrew90 <frandrewjs@gmail.com>
2021-09-24 00:58:00 +0300

    added transactions markup

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Friendlist from './friendList/Friendlist';
 import Profile from './profile/Profile';
 import StatisticalData from './statisticalData/StatisticalData';
+import TransactionHistory from './transactions/TransactionHistory';
 
 const App = () => {
   return (
     <>
       <Profile />,
       <StatisticalData />,
-      <Friendlist />
+      <Friendlist />,
+      <TransactionHistory />
     </>
   );
 };
---
+++ src/Components/transactions/TransactionHistory.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import data from '../../data';
+
+const TransactionHistory = () => {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {data.transactions.map(transaction => {
+          return (
+            <tr key={transaction.id}>
+              <td>{transaction.type}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
